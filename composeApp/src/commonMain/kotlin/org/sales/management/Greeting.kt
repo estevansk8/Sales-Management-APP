275985095d0cab3783e335a1928994365d812f80
estevan <estevanmartins03@gmail.com>
2025-05-12 23:37:55 -0300

    :tada: feat: initial commit

---
+++ composeApp/src/commonMain/kotlin/org/sales/management/Greeting.kt
@@ -0,0 +1,9 @@
+package org.sales.management
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/sales/management/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.sales.management
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
