a63c2c639f5a037fdd1c3d6b55cd0ab681ad95de
estevan <estevanmartins03@gmail.com>
2025-05-15 18:15:46 -0300

    :spakles: feat: started initKoin at iOS

--- composeApp/src/iosMain/kotlin/org/sales/management/KoinHelper.kt
+++ composeApp/src/iosMain/kotlin/org/sales/management/KoinHelper.kt
@@ -1,2 +1,10 @@
 package org.sales.management
 
+import org.koin.core.context.startKoin
+import org.sales.management.core.di.appModules
+
+fun initKoin() {
+    startKoin {
+        modules(appModules)
+    }
+}
