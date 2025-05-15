8151ebbe41855700a15b543a0f833e8435fc4d43
estevan <estevanmartins03@gmail.com>
2025-05-15 17:56:06 -0300

    :sparkles: feat: added BaseApp to DI

---
+++ composeApp/src/androidMain/kotlin/org/sales/management/BaseApp.kt
@@ -0,0 +1,12 @@
+package org.sales.management
+
+import android.app.Application
+import io.ktor.http.ContentType
+
+class BaseApp : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
--- composeApp/src/commonMain/kotlin/org/sales/management/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.sales.management
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
