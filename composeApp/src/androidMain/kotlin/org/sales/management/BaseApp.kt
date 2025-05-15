e1c9fd3f28c02d9f5b4c2ab80a0aa781552f3b48
estevan <estevanmartins03@gmail.com>
2025-05-15 18:06:28 -0300

    :sparkles: feat: startKoin at BaseApp Android

--- composeApp/src/androidMain/kotlin/org/sales/management/BaseApp.kt
+++ composeApp/src/androidMain/kotlin/org/sales/management/BaseApp.kt
@@ -1,12 +1,18 @@
 package org.sales.management
 
 import android.app.Application
-import io.ktor.http.ContentType
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.sales.management.core.di.appModules
 
 class BaseApp : Application(){
 
     override fun onCreate() {
         super.onCreate()
 
+        startKoin{
+            androidContext(this@BaseApp)
+            modules(appModules)
+        }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/org/sales/management/Platform.kt
@@ -0,0 +1,7 @@
+package org.sales.management
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
