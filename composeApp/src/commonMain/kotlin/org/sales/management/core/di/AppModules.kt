86fba7cb11abc97b089341f77f06ee4902aa992c
estevan <estevanmartins03@gmail.com>
2025-05-16 01:22:00 -0300

    :sparkles: feat: dataModule added at AppModules

--- composeApp/src/commonMain/kotlin/org/sales/management/core/di/AppModules.kt
+++ composeApp/src/commonMain/kotlin/org/sales/management/core/di/AppModules.kt
@@ -3,7 +3,27 @@ package org.sales.management.core.di
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import org.sales.management.clients.data.remote.ClientService
+import org.sales.management.clients.domain.repository.ClientRepository
 import org.sales.management.clients.presentation.list.ListClientsViewModel
+import org.sales.management.clients.data.repositories.ClientRepositoryImpl
+import org.sales.management.core.ktor.buildHttpClient
+import org.sales.management.core.ktor.getHttpEngine
+
+
+val dataModule = module {
+    single<ClientService> {
+        ClientService(
+            httpClient = buildHttpClient(getHttpEngine())
+        )
+    }
+
+    single<ClientRepository> {
+        ClientRepositoryImpl(
+            service = get()
+        )
+    }
+}
 
 val viewModelModule = module {
     viewModel {
