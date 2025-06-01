38de1470cd7adb3c5a69bc0cda1e6ae47d714a74
estevan <estevanmartins03@gmail.com>
2025-06-01 16:54:30 -0300

    :recycle: refact: added field to image at LoginScreen

--- composeApp/src/commonMain/kotlin/org/sales/management/auth/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/org/sales/management/auth/login/LoginScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -25,6 +26,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.input.key.Key.Companion.R
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import managementsalesapp.composeapp.generated.resources.Res
+import managementsalesapp.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
@@ -38,11 +41,11 @@ fun LoginScreen(onLogin: () -> Unit) {
     ) {
         Spacer(modifier = Modifier.height(32.dp))
 
-//        Image(
-//            painter = painterResource(),
-//            contentDescription = null,
-//            modifier = Modifier.size(200.dp)
-//        )
+        Image(
+            painter = painterResource(Res.drawable.compose_multiplatform),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
 
         Spacer(modifier = Modifier.height(32.dp))
 
@@ -58,8 +61,9 @@ fun LoginScreen(onLogin: () -> Unit) {
                 Text(
                     text = "Bem vindo\nnovamente\nEmpreendedor!",
                     fontWeight = FontWeight.Bold,
-                    fontSize = 40.sp,
-                    textAlign = TextAlign.Start
+                    fontSize = 44.sp,
+                    textAlign = TextAlign.Start,
+                    lineHeight = 40.sp,
                 )
 
                 Spacer(modifier = Modifier.height(24.dp))
