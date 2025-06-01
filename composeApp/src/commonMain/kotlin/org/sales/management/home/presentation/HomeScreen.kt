e9ce856806bb7bf93f291ac10858e562661bdec5
estevan <estevanmartins03@gmail.com>
2025-06-01 13:35:06 -0300

    :recycle: refact: HomeScreen ui

--- composeApp/src/commonMain/kotlin/org/sales/management/home/presentation/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/sales/management/home/presentation/HomeScreen.kt
@@ -6,10 +6,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import org.sales.management.home.presentation.itens.FeatureCard
 import org.sales.management.home.presentation.itens.SalesCard
 import androidx.compose.material3.Text
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import org.sales.management.home.domain.Feature
@@ -43,13 +52,58 @@ fun HomeScreen(
             verticalArrangement = Arrangement.Top,
             modifier = Modifier.fillMaxSize()
         ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp, start = 16.dp, end = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Olá\nEstevan!",
+                    fontSize = 24.sp,
+                    modifier = Modifier.weight(1f)
+                )
+
+                Box(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(CircleShape)
+                        .background(Color.Gray)
+                ){
+                    Icon(
+                        imageVector = androidx.compose.material.icons.Icons.Default.Notifications,
+                        contentDescription = "Perfil",
+                        tint = Color.White,
+                        modifier = Modifier.padding(8.dp).align(Alignment.Center)
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(4.dp))
+
+                Box(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(CircleShape)
+                        .background(Color.Gray)
+                ){
+                    Icon(
+                        imageVector = androidx.compose.material.icons.Icons.Default.Person,
+                        contentDescription = "Perfil",
+                        tint = Color.White,
+                        modifier = Modifier.padding(8.dp).align(Alignment.Center)
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
             SalesCard {  }
 
             Text(
                 text = "Meu negócio",
-                fontSize = 16.sp,
+                fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.Start).padding(start = 16.dp)
+                modifier = Modifier.align(Alignment.Start).padding(start = 16.dp, bottom = 8.dp)
             )
 
             Row(
--- composeApp/src/commonMain/kotlin/org/sales/management/home/presentation/itens/SalesCard.kt
+++ composeApp/src/commonMain/kotlin/org/sales/management/home/presentation/itens/SalesCard.kt
@@ -1,21 +1,31 @@
 package org.sales.management.home.presentation.itens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,21 +45,40 @@ fun SalesCard(
             ),
             modifier = Modifier
                 .fillMaxWidth()
-                .height(130.dp)
-                .padding(horizontal = 40.dp, vertical = 8.dp)
+                .height(160.dp)
+                .padding(horizontal = 20.dp, vertical = 8.dp)
                 .padding(bottom = 16.dp)
         ) {
             Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.fillMaxSize()
+                verticalAlignment = Alignment.Top,
+                modifier = Modifier.fillMaxSize().padding(top = 16.dp)
             ) {
                 Text(
                     text = "Vender",
                     color = MaterialTheme.colorScheme.onPrimary,
                     fontSize = 36.sp,
                     fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(start = 16.dp).align(Alignment.CenterVertically)
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .weight(1f)
                 )
+
+                Box(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.primary)
+                ){
+                    Icon(
+                        imageVector = androidx.compose.material.icons.Icons.Default.Add,
+                        contentDescription = "add",
+                        tint = Color.White,
+                        modifier = Modifier.padding(8.dp).align(Alignment.Center)
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(12.dp))
+
             }
         }
     }
