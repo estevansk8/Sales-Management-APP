d79ef7a49fb6ce1f19709f273a4029ed37043d13
estevan <estevanmartins03@gmail.com>
2025-06-03 00:20:13 -0300

    :recycle: refact: changed return of AuthRepository functions

--- composeApp/src/commonMain/kotlin/org/sales/management/auth/domain/repository/AuthRepository.kt
+++ composeApp/src/commonMain/kotlin/org/sales/management/auth/domain/repository/AuthRepository.kt
@@ -1,6 +1,9 @@
 package org.sales.management.auth.domain.repository
 
+import org.sales.management.auth.domain.model.CreateUserResponse
+import org.sales.management.auth.domain.model.LoginUserResponse
+
 interface AuthRepository {
-    suspend fun login(email: String, password: String): AuthResult<String>
-    suspend fun signUp(name: String, email: String, password: String, ): AuthResult<String>
+    suspend fun login(email: String, password: String): LoginUserResponse
+    suspend fun signUp(name: String, email: String, password: String, ): CreateUserResponse
 }
