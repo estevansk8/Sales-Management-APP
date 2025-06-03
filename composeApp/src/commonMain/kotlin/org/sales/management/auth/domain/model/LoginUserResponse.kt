d7dbfb97ff6005cda0094881ca6cb8241ede14a4
estevan <estevanmartins03@gmail.com>
2025-06-03 08:44:50 -0300

    :recycle: refact: LoginUserResponse correct fields

--- composeApp/src/commonMain/kotlin/org/sales/management/auth/domain/model/LoginUserResponse.kt
+++ composeApp/src/commonMain/kotlin/org/sales/management/auth/domain/model/LoginUserResponse.kt
@@ -4,6 +4,13 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class LoginUserResponse(
+    val success: Boolean,
+    val message: String,
+    val data: LoginUserData
+)
+
+@Serializable
+data class LoginUserData(
     val userName : String,
     val accessToken: String,
     val refreshToken: String
