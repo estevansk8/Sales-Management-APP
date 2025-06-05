c896d82eef6813e05bce7a2e5212e224027c8f15
estevan <estevanmartins03@gmail.com>
2025-06-05 00:30:05 -0300

    :sparkles: feat: GetPreferencesModule on androidMain

--- composeApp/src/androidMain/kotlin/org/sales/management/core/data/local/datastore/GetPreferenceModule.kt
+++ composeApp/src/androidMain/kotlin/org/sales/management/core/data/local/datastore/GetPreferenceModule.kt
@@ -1,2 +1,9 @@
 package org.sales.management.core.data.local.datastore
 
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual val preferenceModule: Module = module {
+    single { createDataStore(androidContext()) }
+}
