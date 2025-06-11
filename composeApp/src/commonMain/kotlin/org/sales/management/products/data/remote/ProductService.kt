ba40fb065d8c8d2df1b53ff6897891634070359a
estevan <estevanmartins03@gmail.com>
2025-06-11 10:08:49 -0300

    :sparkles: feat:ProductService with routes

--- composeApp/src/commonMain/kotlin/org/sales/management/products/data/remote/ProductService.kt
+++ composeApp/src/commonMain/kotlin/org/sales/management/products/data/remote/ProductService.kt
@@ -1,4 +1,53 @@
 package org.sales.management.products.data.remote
 
-class ProductService {
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.HttpStatusCode
+import org.sales.management.clients.domain.model.ClientDTO
+import org.sales.management.core.data.remote.ktor.ApiConstants
+import org.sales.management.products.domain.model.Product
+import org.sales.management.products.domain.model.ProductResponse
+import org.sales.management.products.domain.model.ProductRequest
+
+class ProductService (
+    private val httpClient: HttpClient
+){
+    private val baseUrl = "${ApiConstants.BASE_URL}/products"
+
+    suspend fun getProducts(): List<Product>? {
+        val response : HttpResponse = httpClient.get(baseUrl)
+
+        println(response.status.toString())
+
+        return when (response.status){
+            HttpStatusCode.OK -> {
+                val productsResponse : ProductResponse = response.body()
+                return productsResponse.data
+            }
+            else -> null
+        }
+    }
+
+    suspend fun createProduct(request: ProductRequest): Product {
+        val response: HttpResponse = httpClient.post(baseUrl) {
+            setBody(request)
+        }
+
+        return when (response.status) {
+            HttpStatusCode.OK -> {
+                response.body()
+            }
+
+            else -> throw Exception("Erro ao criar produto")
+        }
+    }
+
+    suspend fun deleteProduct(id: Long) {
+        val response: HttpResponse = httpClient.delete("$baseUrl/$id")
+    }
 }
