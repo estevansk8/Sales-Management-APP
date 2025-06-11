9daf351bad682371490fc242416c416630696d52
estevan <estevanmartins03@gmail.com>
2025-06-11 09:49:42 -0300

    :sparkles: feat: ProductListScreen created

---
+++ composeApp/src/commonMain/kotlin/org/sales/management/products/presentation/list/ProductsListScreen.kt
@@ -0,0 +1,88 @@
+package org.sales.management.products.presentation.list
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.koin.compose.viewmodel.koinViewModel
+import org.sales.management.clients.presentation.list.ClientItem
+import org.sales.management.clients.presentation.list.ClientsListViewModel
+
+@Composable
+fun ProductsListScreen(
+    goToClientForm: () -> Unit,
+    viewModel: ProductsListViewModel = koinViewModel()
+) {
+    val products = viewModel.products
+    val isLoading = viewModel.isLoading
+
+    LaunchedEffect(Unit) {
+        viewModel.listProducts()
+        println(clients)
+    }
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                backgroundColor = MaterialTheme.colorScheme.primary,
+                onClick = {
+                    goToClientForm()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Adicionar Cliente",
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        },
+    ){
+        if (isLoading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center),
+                    strokeWidth = 2.dp
+                )
+            }
+        } else if(viewModel.products.isEmpty()){
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "Nenhum produto encontrado!")
+                }
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                items(products) { product ->
+                    ClientItem(product)
+                    Divider(modifier = Modifier.padding(vertical = 8.dp))
+                }
+            }
+        }
+    }
+
+
+}
