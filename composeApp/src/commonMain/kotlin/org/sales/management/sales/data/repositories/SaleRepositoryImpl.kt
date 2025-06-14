a94e8953024b0ec009ac13a198936a8ab6ca1a7e
estevan <estevanmartins03@gmail.com>
2025-06-13 21:45:44 -0300

    :sparkles: feat: SaleService created

--- composeApp/src/commonMain/kotlin/org/sales/management/sales/data/repositories/SaleRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/sales/management/sales/data/repositories/SaleRepositoryImpl.kt
@@ -1,4 +1,6 @@
 package org.sales.management.sales.data.repositories
 
-class SaleRepositoryImpl {
+import org.sales.management.sales.domain.repository.SaleRepository
+
+class SaleRepositoryImpl: SaleRepository {
 }
