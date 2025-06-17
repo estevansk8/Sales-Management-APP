4c8f92ce570a6f7e965957ba2e9297bc3ec6b87c
estevan <estevanmartins03@gmail.com>
2025-06-17 10:33:20 -0300

    :sparkles: feat: added SaleItemRow

---
+++ composeApp/src/commonMain/kotlin/org/sales/management/sales/presentation/form/components/SaleItemRow.kt
@@ -0,0 +1,54 @@
+package org.sales.management.sales.presentation.form.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.sales.management.sales.domain.model.SaleItem
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.text.font.FontWeight
+
+
+
+@Composable
+fun CartItemRow(
+    item: SaleItem,
+    onQuantityChange: (Int) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        // 1. Controle de Quantidade
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            IconButton(onClick = { onQuantityChange(-1) }) {
+                Icon(Icons.Default.Remove, "Diminuir")
+            }
+            Text(item.quantity.toString(), style = MaterialTheme.typography.h4)
+            IconButton(onClick = { onQuantityChange(1) }) {
+                Icon(Icons.Default.Add, "Aumentar")
+            }
+        }
+
+        // 2. Nome e Preço Unitário
+        Column(modifier = Modifier.weight(1f).padding(horizontal = 8.dp)) {
+            Text(item.name, fontWeight = FontWeight.Bold)
+            Text("R$ ${item.unitPrice}", style = MaterialTheme.typography.h5)
+        }
+
+        // 3. Preço Total do Item
+        Text("R$ ${item.subtotal}", fontWeight = FontWeight.SemiBold)
+    }
+}
