bdd261fcea56b3f8ea88867d0d2d8f0412f8364b
estevan <estevanmartins03@gmail.com>
2025-06-17 09:53:02 -0300

    :recycle: refactor: changed implementation to ProductRepositoryImpl

--- composeApp/src/commonMain/kotlin/org/sales/management/products/data/repositories/ProductRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/sales/management/products/data/repositories/ProductRepositoryImpl.kt
@@ -1,4 +1,35 @@
 package org.sales.management.products.data.repositories
 
-class ProductRepositoryImpl {
+import org.sales.management.products.data.remote.ProductService
+import org.sales.management.products.domain.model.Product
+import org.sales.management.products.domain.model.ProductRequest
+import org.sales.management.products.domain.repository.ProductRepository
+
+class ProductRepositoryImpl(
+    private val service: ProductService
+): ProductRepository {
+
+    override suspend fun getAllProducts(): List<Product>? {
+        return service.getProducts()
+    }
+
+    override suspend fun getProductById(id: Long): Product? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getProductsByName(name: String): List<Product>? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createProduct(request: ProductRequest): Product {
+        return service.createProduct(request)
+    }
+
+    override suspend fun updateProduct(product: Product): Product {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteProduct(id: Long) {
+        TODO("Not yet implemented")
+    }
 }
